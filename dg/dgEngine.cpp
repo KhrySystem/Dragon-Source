357052e634ede8e29f22fe3ab76f32c429d73fbf
Comp01-sudo <70709374+Comp01-sudo@users.noreply.github.com>
2022-10-05 09:37:19 -0400

    Major changes to the internals of the Engine

--- dg/dgEngine.cpp
+++ dg/dgEngine.cpp
@@ -1,26 +1,41 @@
 #include <dragon/dragon.hpp>
 
+std::string dgEngine::appName;
+std::vector<GLFWwindow*> dgEngine::windows;
+
 DGAPI DG_BOOL dgCreateNewWindow(DgWindowCreateParams params) {
 	GLFWwindow* window;
-	
-	if(DgWindowCreateParams::shareResources && dgEngine::windows.size() > 0)
-		window = glfwCreateWindow(
-			params.width, params.height,
-			dgEngine::appName.c_str(), 
-			NULL,
-			dgEngine::windows.at(0)
-		);
-	else 
+	if(dgEngine::windows.size() > 0) {
 		window = glfwCreateWindow(
 			params.width, params.height,
 			dgEngine::appName.c_str(),
 			NULL,
-			NULL
+			dgEngine::windows.at(0)
 		);
-	
+	} else {
+		window = glfwCreateWindow(
+			params.width, params.height,
+			dgEngine::appName.c_str(),
+			NULL, NULL
+		);
+	}
+
 	if(window == NULL)
-		return DG_FALSE;
+		throw dgGLFWWindowCreationFailedException() << DgExceptionInfo("GLFW window handle was NULL.");
 	
 	dgEngine::windows.push_back(window);
 	return DG_TRUE;
+}
+
+DGAPI DG_BOOL dgShouldWindowClose(int index) {
+	printf("Should window %i close: %i\n", index, glfwWindowShouldClose(dgEngine::windows.at(index)));
+	return glfwWindowShouldClose(dgEngine::windows.at(index));
+}
+
+DGAPI void dgUpdate() {
+	glfwPollEvents();
+}
+
+DGAPI void dgCloseWindow(int index) {
+	glfwDestroyWindow(dgEngine::windows.at(index));
 }
---
+++ dg/internal.cpp
@@ -0,0 +1,5 @@
+#include <dragon/dragon.hpp>
+
+DGAPI std::string concancate(std::string s1, std::string s2) {
+	return s1 + s2;
+}
